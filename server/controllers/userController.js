6eaadf20acd2930a1b7005d9f3fb444eee259be4
Saikat <saikatchakraborty416@gmail.com>
2025-05-18 00:50:16 +0530

    .

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -74,7 +74,10 @@ exports.login = async (req, res) => {
         
         const options={
             expires:new Date(Date.now()+3*24*60*60*1000),
-            httpOnly:true
+            httpOnly:true,
+            secure:true,
+            sameSite:'None',
+            token,
         }
         user.password=undefined;
         user.token=token
